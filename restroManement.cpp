fcfc9d94087414b5440a70e08c11906234c89477
KashyapAhir <kashyapahir786@gmail.com>
2024-05-02 12:20:41 +0530

    change

--- restroManement.cpp
+++ restroManement.cpp
@@ -7,10 +7,12 @@ class Restrorunt{
     public:
     int arr[5]={240,160,120,30,400};
     char item;
-    int qty1[1]; int qty2[1]; int qty3[1]; int qty4[1]; int qty5[1];
+    int qty[1]; int qty2[1]; int qty3[1]; int qty4[1]; int qty5[1];
     int q;
-    int sum=0;
-    int total=0;
+    int sum1=0; int sum2=0; int sum3=0; int sum4=0; int sum5=0;
+    int Total=0;
+    int GST;
+    int SubTotal=0;
 
 
     void menu();
@@ -48,10 +50,9 @@ void Restrorunt :: order(){
 
                for(int i=0; i<1; i++){
                    cout<< endl <<"enter pizza quantity do you want? ";
-                   cin >> qty1[i];
-                   sum=qty1[i]*arr[0];
+                   cin >> qty[i];
+                   
                }
-               cout << endl << "Total :-" << sum;
 
 
        break;
@@ -61,9 +62,7 @@ void Restrorunt :: order(){
                 for(int i=0; i<1; i++){
                    cout<< endl <<"enter Burger quantity do you want? ";
                    cin >> qty2[i];
-                   sum=qty2[i]*arr[1];
                }
-               cout << endl << "Total :-" << sum;
 
         break;
 
@@ -72,9 +71,7 @@ void Restrorunt :: order(){
                for(int i=0; i<1; i++){
                    cout<< endl <<"enter Green Sandwich quantity do you want? ";
                    cin >> qty3[i];
-                   sum=qty3[i]*arr[2];
                }
-               cout << endl << "Total :-" << sum;
 
         break;
 
@@ -83,21 +80,15 @@ void Restrorunt :: order(){
                for(int i=0; i<1; i++){
                    cout<< endl <<"enter Cold quantity do you want? ";
                    cin >> qty4[i];
-                   sum=qty4[i]*arr[3];
                }
 
-
-               cout << endl << "Total :-" << sum;
         break;
 
         case '5':
                for(int i=0; i<1; i++){
-                   cout<< endl <<"enter Cold quantity do you want? ";
+                   cout<< endl <<"enter Biryani quantity do you want? ";
                    cin >> qty5[i];
-                   sum=qty5[i]*arr[4];
                }
-
-               cout << endl << "Total :-" << sum;
         break;
     }
 
@@ -107,8 +98,52 @@ void Restrorunt :: order(){
 
 void Restrorunt :: bill(){
 
-     
+     cout << endl << "=======================Bill======================" << endl;
 
+     cout << endl << "Item       quntity          Price         Amount"<< endl;
+     cout << "________________________________________________________"<<endl;
+     
+     for(int i=0; i<1; i++){
+         
+        sum1=qty[i]*arr[0];
+        sum2=qty2[i]*arr[1];
+        sum3=qty3[i]*arr[2];
+        sum4=qty4[i]*arr[3];
+        sum5=qty5[i]*arr[4];
+        if(qty[i]>=20){
+            Total=sum2+sum3+sum4+sum5;
+        }
+        else{
+            Total=sum1+sum2+sum3+sum4+sum5;
+        }
+        
+        GST=Total*18/100;
+        SubTotal=Total+GST;
+         
+         if(qty[i]<=20){
+             cout << "pizza" << "\t\t\t" << qty[i] << "\t\t\t"<< arr[0] << "\t\t\t"<<sum1<<endl;
+         }
+         if(qty2[i]>=1){
+         cout << "Burger" << "\t\t\t" << qty2[i] << "\t\t\t"<< arr[1] << "\t\t\t"<<sum2<<endl;
+         }
+         if(qty3[i]>=1){
+         cout << "Green Sandwich" << "\t" << qty3[i] << "\t\t\t"<< arr[2] << "\t\t\t"<<sum3<<endl;
+         }
+         if(qty4[i]>=1){
+         cout << "Cold" << "\t\t\t" << qty4[i] << "\t\t\t"<< arr[3] << "\t\t\t"<<sum4<<endl;
+         }
+         if(qty5[i]>=1){
+         cout << "Biryani" << "\t\t\t" << qty5[i] << "\t\t\t"<< arr[4] << "\t\t\t"<<sum5<<endl;
+         }
+         cout << "________________________________________________________"<<endl;
+         cout << "                                  Total    : "<<Total << endl;
+         cout << "                                  GST 18%  : "<< GST << endl;
+         cout << "________________________________________________________"<<endl;
+         cout << "                                  SubTotal  : "<< SubTotal << endl;
+
+    } 
+    
+ 
 };
 
 
@@ -144,6 +179,7 @@ int main(){
 
             case '3':
                 "3. For Bill \n";
+                obj.bill();
                 break;
 
             case '4':
